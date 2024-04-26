08f89ce5ea26d1b6bf397c1ce289eea9e08fa621
eb2197 <eb2197@york.ac.uk>
2024-04-26 13:17:34 +0100

    clock exercise nearly finished!
    up to 3.6 of the clock exercise W7P1

--- stage3/main.js
+++ stage3/main.js
@@ -1,7 +1,25 @@
 function updateClock(clockID, hoursDiff){
     const time = new Date();
-    console.log(time);
+    const hours24 = (time.getHours() + hoursDiff) % 24;
+    const hours12 = hours24 % 12;
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    const clock = document.getElementById(clockID);
+
+    let hourHand = clock.getElementsByClassName("hour")[0];
+    hourHand.style.transform = "translateX(100px) rotate(" + rotateHand(hours12) + "deg)";
+
+    clock.getElementsByClassName("am-pm")[0].innerHTML = hours24 > 12 ? "PM" : "AM";
+
+     console.log(time, hours24, hours12, minutes, seconds);
+    //console.log(hourHand.style);
+}
+
+function rotateHand(hour){
+    return hour / 12 * 360;
 }
 
 updateClock("york", 0);
 updateClock("sydney", 11);
+
+
